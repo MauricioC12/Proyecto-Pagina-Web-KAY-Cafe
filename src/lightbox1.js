045bbc2f926594224ec39c2ec9cda6439415d570
mauricioc12 <mauriciocontrerasr1202@gmail.com>
2022-05-20 12:00:23 -0500

    Se subió el proyecto

---
+++ src/lightbox1.js
@@ -0,0 +1,22 @@
+const imagenes = document.querySelectorAll('.imagen-producto1')
+const imagenesLight = document.querySelector('.agregar-imagen1')
+const contenedorLight = document.querySelector('.imagen-light1')
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+        aparecerImagen(imagen.getAttribute('src'))
+    })
+})
+
+contenedorLight.addEventListener('click', (e)=>{
+    if(e.target !== imagenesLight){
+        contenedorLight.classList.toggle('show')
+        imagenesLight.classList.toggle('showImage')
+    }
+})
+
+const aparecerImagen = (imagen)=>{
+    imagenesLight.src = imagen
+    contenedorLight.classList.toggle('show')
+    imagenesLight.classList.toggle('showImage')
+}
